ece2ed2c3172f8c07c99fadb9d93e9513fd049cf
Micah Palmquist <micah.palmquist@gmail.com>
2026-03-26 20:47:13 -0400

    feat: update notification email for Farmdale ESL Network registration

--- public/registration-configs/esl-network-registration-farmdale.js
+++ public/registration-configs/esl-network-registration-farmdale.js
@@ -12,7 +12,7 @@ window.REGISTRATION_FORM_CONFIGS['Farmdale ESL Network Registration'] = {
       farmdaleEslRegistrationSubtitle: 'Inscripción abierta para el semestre de primavera de 2026 --- cupos limitados del 11 de abril al 6 de junio (Sábados)\n¿Preguntas? Llame o envíe un mensaje de texto a Joseph al: 407-883-4417',
     },
   },
-  NotificationEmail: 'micah.palmquist@gmail.com',
+  NotificationEmail: 'sweber9990@gmail.com',
   Images: [
     {
       src: 'https://www.jotform.com/uploads/Foushee_Kathy/form_files/coffee%20logo.641d2680802767.02034693.PNG',
